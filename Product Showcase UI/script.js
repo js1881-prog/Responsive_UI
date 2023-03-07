add328b99de0d964210414ca0bc39934be46fff0
padonan <bbde1861@gmail.com>
2023-03-08 04:38:38 +0900

    03 08

--- Product Showcase UI/script.js
+++ Product Showcase UI/script.js
@@ -1,15 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
-    navigator: {
-        nextEl: ".swiper-next-button",
-        prevEl: ".swiper-prev-button"
-    },
-    effect: "fade",
+	navigation: {
+		nextEl: ".swiper-next-button",
+		prevEl: ".swiper-prev-button"
+	},
+	effect: "fade",
 	loop: "infinite",
 	pagination: {
           el: ".swiper-pagination",
           type: "fraction",
         }
 });
+
 swiper.on('slideChange', function(sld) {
 	document.body.setAttribute('data-sld', sld.realIndex);
 })
